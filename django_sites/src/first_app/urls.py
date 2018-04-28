958655ff1fe3096cf53729d3179889e763c19436
renshangui <renshangui@163.com>
2018-04-28 18:05:26 +0800

    修改docker配置&restful create

--- django_sites/src/first_app/urls.py
+++ django_sites/src/first_app/urls.py
@@ -10,4 +10,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^user/new', views.CreateAPIView.as_view(), name='user_new'),
+    url(r'^user/(?P<name>\w+)', views.UserDetailAPIView.as_view(), name='user_name')
 ]
--- django_sites/src/run-site.sh
+++ django_sites/src/run-site.sh
@@ -4,6 +4,8 @@ source env.sh
 resolve_service ${POSTGRESQL_HOST}:${POSTGRESQL_PORT}
 wait_tcp_dependency ${RESOLVE_IP} ${RESOLVE_PORT}
 
+# collect static files
+python manage.py collectstatic --no-input
 # do database migration
 python manage.py makemigrations
 python manage.py migrate
